c59ed4c1268e3de77bd9d4547bdb975570fe50d3
ReshmaSJ <reshmasj94@outlook.com>
2025-06-06 16:20:44 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! ');
+  res.send('NODEJSAPPP ');
 });
 
 app.listen(port, () => {
-  console.log(`App is running at http://localhost:${port}`);
+  console.log(`App is running at http://localhost:${port}`)
 });
