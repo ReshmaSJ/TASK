df7c9ca09bcb50eb47d5b9236db9a0b2cf8c16c1
ReshmaSJ <reshmasj94@outlook.com>
2025-07-07 13:36:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello dev1321');});
+  res.send('hello dev1321');
+});
 
 app.listen(port, () => {
   console.log(`App is running at http://localhost:${port}`)
