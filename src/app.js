2c2404bde36b540ff5634eef3ffe5b7a2491a88a
ReshmaSJ <reshmasj94@outlook.com>
2025-07-07 13:22:42 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,27 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-#app.get('/', (req, res) => {
-  #res.send('hello dev1321');});
+app.get('/', (req, res) => {
+  res.send('hello dev1321');});
 
-#app.listen(port, () => {
-  #console.log(`App is running at http://localhost:${port}`)
-#});
-
-
-
-
-const client = require('prom-client');
-const collectDefaultMetrics = client.collectDefaultMetrics;
-collectDefaultMetrics();
-
-app.get('/metrics', async (req, res) => {
-  res.send('hello dev1242');
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`)
 });
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
-});
+
+
+
 
 
 
